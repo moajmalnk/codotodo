324ba6517b6343d44f0d5d36eb7b9ff55bdcde20
Ajmalnk1 <ai.codomail@gmail.com>
2025-01-27 02:36:48 +0530

    Initial commit

---
+++ public/service-worker.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'codo-todo-v1';
+const STATIC_ASSETS = [
+  '/',
+  '/index.html',
+  '/static/css/main.chunk.css',
+  '/static/js/main.chunk.js',
+  '/manifest.json',
+  '/favicon.ico',
+  '/logo192.png',
+  '/offline.html'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(STATIC_ASSETS);
+      })
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Fetch Event Strategy
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached version if found
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        // Try to fetch new version
+        return fetch(fetchRequest)
+          .then((response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            // Add new version to cache
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            // If fetch fails, return offline page
+            if (event.request.mode === 'navigate') {
+              return caches.match('/offline.html');
+            }
+          });
+      })
+  );
+}); 
